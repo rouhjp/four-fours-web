545e79ca233cfd9a0ed92308d551d3829f7a8ce2
Rouh <roaming.new@gmail.com>
2025-03-16 10:41:02 +0900

    レスポンシブスタイル調整

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,27 @@ function App() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-gray-800 mb-8">Four Fours</h1>
-      <div className="relative bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-        {mode === "Free" && (
-          <>
-            <FreeModeCard />
-            <FaChevronRight
-              className="absolute top-1/2 right-[-30px] transform -translate-y-1/2 text-gray-500 w-6 h-6 cursor-pointer"
-              onClick={() => setMode("Solve")}
-            />
-          </>
-        )}
-        {mode === "Solve" && (
-          <>
-            <SolveModeCard />
+      <div className="flex items-center justify-center mb-8 w-full">
+        <div className="w-8">
+          {mode==="Solve" && (
             <FaChevronLeft
-              className="absolute top-1/2 left-[-30px] transform -translate-y-1/2 text-gray-500 w-6 h-6 cursor-pointer"
+              className=" text-gray-500 w-6 h-6 cursor-pointer"
               onClick={() => setMode("Free")}
             />
-          </>
-        )}
+          )}
+        </div>
+        <div className="relative bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
+          {mode==="Solve" && <SolveModeCard />}
+          {mode==="Free" && <FreeModeCard />}
+        </div>
+        <div className="w-8">
+          {mode==="Free" && (
+            <FaChevronRight
+              className="text-gray-500 w-6 h-6 cursor-pointer"
+              onClick={() => setMode("Solve")}
+            />
+          )}
+        </div>
       </div>
 
       <p className="mt-8 text-gray-500 text-center">
