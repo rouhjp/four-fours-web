4016ed812e3b92126c212fb756cb27f7dc45e302
Rouh <roaming.new@gmail.com>
2026-01-16 00:45:21 +0900

    vitest に変更

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-import { Config } from "jest";
-
-const config: Config = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-};
-
-export default config;
--- src/tests/calculations.test.ts
+++ src/tests/calculations.test.ts
@@ -1,3 +1,4 @@
+import { describe, test, expect } from "vitest";
 import { getAnswer } from "../core/answers";
 import { evaluate } from "../core/calculations";
 
